90dd7b956b113465de8097c9dea519a0cbbae95c
Gleb Nemirich <133893209+Nemirich@users.noreply.github.com>
2024-12-20 18:14:24 +0300

    Add files via upload

---
+++ move.js
@@ -0,0 +1,11 @@
+document.querySelector('.d1').addEventListener('click', function () {
+    window.location.href = "day1.html";
+  });
+
+document.querySelector('.d2').addEventListener('click', function () {
+    window.location.href = "day2.html";
+  });
+
+document.querySelector('.d3').addEventListener('click', function () {
+    window.location.href = "day3.html";
+  });
